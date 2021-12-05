1cabda6ec701eb2378b0b81d1b4cd28e93cec92f
Victor Medina <vamedinac@icloud.com>
2021-12-05 17:19:47 -0500

    routes_edit_delete

--- App.py
+++ App.py
@@ -2,13 +2,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#ruta principal
 @app.route('/')
 def Index():
  return'Hello World'
-
+#ruta add contacto
 @app.route('/add_contact')
 def add_contact():
- return'Agregar contacto'
+ return'Ad Contact'
+
+@app.route('/edit')
+def edit_contact():
+ return 'edit contact'
+
+@app.route('/delete')
+def delete_contact():
+ return 'delete'
 
 if __name__ == '__main__':
 ##uso debug para que cada vez que se haga cambio el servidor renicie
