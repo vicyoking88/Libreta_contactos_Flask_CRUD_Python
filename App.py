5731803f271bab47b933bcd372f7b5dfc74c69b6
Victor Medina <vamedinac@icloud.com>
2021-12-05 15:54:02 -0500

    route_contact

--- App.py
+++ App.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def Index():
  return'Hello World'
 
+@app.route('/add_contact')
+def add_contact():
+ return'Agregar contacto'
+
 if __name__ == '__main__':
 ##uso debug para que cada vez que se haga cambio el servidor renicie
  app.run(port=3000, debug=True)
