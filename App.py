835a2e12d0f2bc9ce0111dd064029e75d7a5bd44
Victor Medina <vamedinac@icloud.com>
2021-12-05 15:51:57 -0500

    route_pri_hello_world

--- App.py
+++ App.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def Index():
+ return'Hello World'
+
 if __name__ == '__main__':
 ##uso debug para que cada vez que se haga cambio el servidor renicie
  app.run(port=3000, debug=True)
